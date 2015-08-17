f3286e73adf469aa95ab3377b9a7c2a898ce81b6
gsmbaku <gsmbaku1@gmail.com>
2015-08-17 08:47:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
- 	var count = prompt("Enter a number: ");
+ 	var count = parseInt(prompt("Enter a number: "));
 	for (var i = 1; i < count; i++){ 
 		
 		if(i % 3 == 0 && i % 5 == 0){
